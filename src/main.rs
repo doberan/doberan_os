64ba3054274506af8d699c3c967878fe385ee564
doberan <doberankei@gmail.com>
2021-01-27 04:27:52 +0900

    [fix] implementation println, print and panic

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,16 @@ mod vga_buffer;
 
 /// この関数はパニック時に呼ばれる
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // この関数の名前修飾をしない
 pub extern "C" fn _start() -> ! {
     // リンカはデフォルトで `_start` という名前の関数を探すので、
     // この関数がエントリポイントとなる
-    // let vga_buffer = 0xb8000 as *mut u8;
-
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
-
-    vga_buffer::print_something();
-
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop{}
 }
